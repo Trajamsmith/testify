759d594d87dba127a687128f6cdadbe142f5fd9d
Travis James Smith <trajamsmith@gmail.com>
2018-08-02 14:10:05 -0500

    Add initial testing

--- src/App.test.js
+++ src/App.test.js
@@ -7,8 +7,11 @@ import App from './App';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App component testing', function() {
+  it('renders welcome message', function() {
+    const wrapper = shallow(<App />);
+    const welcome = <h1 className='App-title'>Welcome to React</h1>;
+    expect(wrapper.contains(welcome)).to.equal(true);
+  })
+})
+
